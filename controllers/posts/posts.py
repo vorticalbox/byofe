f02b9696f86f88b999dc1ecfb90d16abd6ca312f
vorticalbox <vorticalbox@protonmail.com>
2020-11-24 17:44:32 +0000

    add: posts

--- controllers/posts/posts.py
+++ controllers/posts/posts.py
@@ -4,9 +4,10 @@ from datetime import datetime
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from typing import List
-import sqlalchemy
 
 router = APIRouter()
+
+postColumns = ["id", "title", "body", "username", "date"]
 
 
 class PostBody(BaseModel):
@@ -15,12 +16,12 @@ class PostBody(BaseModel):
 
 
 class PostStored(PostBody):
-    ID: int
+    id: int
     date: datetime
     username: str
 
 
-async def savePost(post: PostBody, username: str):
+async def insertPost(post: PostBody, username: str):
     date = datetime.now()
     query = Post.insert().values(username=username,
                                  title=post.title, body=post.body, date=date)
@@ -29,9 +30,25 @@ async def savePost(post: PostBody, username: str):
 
 async def getPosts(skip: int, limit: int):
     query = Post.select().offset(skip).limit(limit)
-    return await database.fetch_all(query)
+    return await database.fetch_one(query)
+
+
+async def getPost(id: int):
+    query = Post.select().where(Post.c.id == id)
+    post = await database.fetch_all(query)
+    return dict(zip(postColumns, post[0]))
 
 
 @router.get('/', response_model=List[PostStored])
-async def Posts(skip: int = 0, limit: int = 20, apiUser = Depends(getUserByApiKey)):
+async def posts(skip: int = 0, limit: int = 20, apiUser=Depends(getUserByApiKey)):
     return await getPosts(skip, limit)
+
+
+@router.get('/{id}', response_model=PostStored)
+async def posts(id: int = 0, apiUser=Depends(getUserByApiKey)):
+    return await getPost(id)
+
+
+@router.post('/')
+async def savePost(body: PostBody, apiUser=Depends(getUserByApiKey)):
+    return await insertPost(body, apiUser)
